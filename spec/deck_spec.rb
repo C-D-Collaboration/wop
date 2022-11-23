ceeb762dcf184833dee67bfc550de218e965c195
Dani Coleman <danicoleman00@gmail.com>
2022-11-23 11:08:57 -0500

    create deck spec; test passing up to last part of i1

---
+++ spec/deck_spec.rb
@@ -0,0 +1,74 @@
+require 'pry'
+require './lib/card'
+require './lib/deck'
+
+RSpec.describe Deck do
+  it "exists" do
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, 'Ace', 14)
+    cards = [card1, card2, card3]
+    deck = Deck.new([cards])
+
+    expect(deck).to be_an_instance_of(Deck)
+  end
+
+  it "has readable attributes" do
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, 'Ace', 14)
+    cards = [card1, card2, card3]
+    deck = Deck.new(cards)
+
+    expect(deck.cards).to be_an(Array)
+    expect(deck.cards).to eq([card1, card2, card3])
+  end
+
+  it "can get rank of card at specific index in array" do
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, 'Ace', 14)
+    cards = [card1, card2, card3]
+    deck = Deck.new(cards)
+
+    expect(deck.cards).to eq([card1, card2, card3])
+    expect(deck.rank_of_card_at(0)).to eq(12)
+    expect(deck.rank_of_card_at(2)).to eq(14)
+  end
+
+  it "can get the high ranking cards in a deck" do
+    # this method will return an array of cards in the deck
+    # that have a rank of 11 or above (face cards and aces)
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, 'Ace', 14)
+    cards = [card1, card2, card3]
+    deck = Deck.new(cards)
+
+    expect(deck.cards).to eq(cards)
+    expect(deck.high_ranking_cards).to eq([card1, card3])
+  end
+
+  it "can get the percent of high ranking cards in the deck" do
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, 'Ace', 14)
+    cards = [card1, card2, card3]
+    deck = Deck.new(cards)
+
+    expect(deck.percent_high_ranking).to eq(66.67)
+  end
+
+  it "can remove the top card from the deck" do
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, 'Ace', 14)
+    cards = [card1, card2, card3]
+    deck = Deck.new(cards)
+
+    expect(deck.remove_card).to eq(card1)
+    expect(deck.cards).to eq([card2, card3])
+    expect(deck.high_ranking_cards).to eq([card3])
+    expect(deck.percent_high_ranking).to eq(50.0)
+  end
+end
