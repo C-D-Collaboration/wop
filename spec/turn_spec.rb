157a942bc48b2f84ee6dd781e3f09a9ec37bea28
Dani Coleman <danicoleman00@gmail.com>
2023-03-15 10:39:31 -0600

    compete iteration 2

--- spec/turn_spec.rb
+++ spec/turn_spec.rb
@@ -374,4 +374,71 @@
       expect(player2.deck.cards).to eq([card7])
     end
   end
+
+  describe "Helper methods" do 
+    it "can get basic type" do 
+      card1 = Card.new(:heart, 'Jack', 11)
+      card2 = Card.new(:heart, '10', 10)    
+      card3 = Card.new(:heart, '9', 9)    
+      card4 = Card.new(:diamond, 'Jack', 11)    
+      card5 = Card.new(:heart, '8', 8)    
+      card6 = Card.new(:diamond, 'Queen', 12)    
+      card7 = Card.new(:heart, '3', 3)    
+      card8 = Card.new(:diamond, '2', 2)    
+
+      deck1 = Deck.new([card1, card2, card5, card8])    
+      deck2 = Deck.new([card3, card4, card6, card7])    
+
+      player1 = Player.new("Megan", deck1)    
+      player2 = Player.new("Aurora", deck2)    
+
+      turn = Turn.new(player1, player2) 
+
+      expect(turn.basic_type).to eq(true)
+    end
+
+    it "can get war type" do 
+      card1 = Card.new(:heart, 'Jack', 11)    
+      card2 = Card.new(:heart, '10', 10)    
+      card3 = Card.new(:heart, '9', 9)    
+      card4 = Card.new(:diamond, 'Jack', 11)    
+      card5 = Card.new(:heart, '8', 8)    
+      card6 = Card.new(:diamond, 'Queen', 12)    
+      card7 = Card.new(:heart, '3', 3)    
+      card8 = Card.new(:diamond, '2', 2)    
+
+      deck1 = Deck.new([card1, card2, card5, card8])    
+      deck2 = Deck.new([card4, card3, card6, card7])    
+
+      player1 = Player.new("Megan", deck1)    
+      player2 = Player.new("Aurora", deck2)    
+
+      turn = Turn.new(player1, player2) 
+
+      expect(turn.basic_type).to eq(false)
+      expect(turn.war_type).to eq(true)
+    end
+
+    it "can get MAD type" do 
+      card1 = Card.new(:heart, 'Jack', 11)    
+      card2 = Card.new(:heart, '10', 10)    
+      card3 = Card.new(:heart, '9', 9)    
+      card4 = Card.new(:diamond, 'Jack', 11)    
+      card5 = Card.new(:heart, '8', 8)    
+      card6 = Card.new(:diamond, '8', 8)    
+      card7 = Card.new(:heart, '3', 3)    
+      card8 = Card.new(:diamond, '2', 2)    
+
+      deck1 = Deck.new([card1, card2, card5, card8])    
+      deck2 = Deck.new([card4, card3, card6, card7])    
+
+      player1 = Player.new("Megan", deck1)    
+      player2 = Player.new("Aurora", deck2)    
+
+      turn = Turn.new(player1, player2)  
+
+      expect(turn.basic_type).to eq(false)
+      expect(turn.mad_type).to eq(true)
+    end
+  end
  end
